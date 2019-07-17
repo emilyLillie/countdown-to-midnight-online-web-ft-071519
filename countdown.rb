63bb7de6e9248730c3b1019a2bcb71a8606fe29d
Emily Lillie <emilylillie3@gmail.com>
2019-07-17 00:47:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,12 @@
-#write your code here
+#write your code here 
 
-def countdown(number)
-  while number > 0 
-  puts "#{number} SECOND(S)!"
-  x -= 1 
-end
-end
+def countdown(num)
 
-countdown
+  while num > 0
+
+    puts "#{num} SECOND(S)!"
+   num -= 1
+
+  end
+   "HAPPY NEW YEAR!"
+ end
